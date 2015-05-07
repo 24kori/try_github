842408702be34074311a4fbd245c18801c5911e7
User <User@H27-MobilePC03>
2015-05-07 17:26:11 +0900

    Addの追加

---
+++ src/try_github/Add.java
@@ -0,0 +1,7 @@
+package try_github;
+public class Add {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+}
--- src/try_github/Main.java
+++ src/try_github/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		System.out.println("Hello Git with Eclipse");
 
 	}
 
